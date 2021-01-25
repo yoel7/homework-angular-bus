6b2c2bd92b6b37c70bb24846fafe7ac6b30ca7f7
yoel7 <76557026+yoel7@users.noreply.github.com>
2021-01-25 19:35:58 +0200

    לא מבין למה הקומפוננטות לא נראות

--- src/app/components/search-trip/search-trip.component.ts
+++ src/app/components/search-trip/search-trip.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 import { bustrip } from 'src/app/models/bustrip.model';
 import { DataService } from 'src/app/Services/data.service';
-
+// import {  } from 'src/app/components/search/search.component'
 @Component({
   selector: 'app-search-trip',
   templateUrl: './search-trip.component.html',
@@ -11,7 +12,7 @@ export class SearchTripComponent implements OnInit {
   dataFiltSeri: bustrip[];
   constructor(private databus: DataService) {}
   ngOnInit(): void {
-    // this.dataFiltSeri = this.databus.bustripfilter;
+    this.dataFiltSeri = this.databus.bustripfilter;
   }
   pusArTr(a?): void {
     this.dataFiltSeri = this.databus.bustripfilter;
