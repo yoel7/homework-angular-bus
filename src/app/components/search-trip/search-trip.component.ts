522c816f9e3d8f8f51faaf53e9ffc8d7aaa163b6
yoel7 <76557026+yoel7@users.noreply.github.com>
2021-02-12 08:08:06 +0200

    סתם

--- src/app/components/search-trip/search-trip.component.ts
+++ src/app/components/search-trip/search-trip.component.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { from, Subscription } from 'rxjs';
 import { bustrip } from 'src/app/models/bustrip.model';
 import { DataService } from 'src/app/Services/data.service';
-
 @Component({
   selector: 'app-search-trip',
   templateUrl: './search-trip.component.html',
   styleUrls: ['./search-trip.component.css'],
 })
 export class SearchTripComponent implements OnInit {
+
+
   dataFiltSeri: bustrip[];
   busServiceSubscription: Subscription;
   constructor(private databus: DataService) {
     
    }
   ngOnInit(): void {
+
     this.busServiceSubscription =this.databus.get().subscribe((a)=>{
      this.dataFiltSeri=a; 
     });
