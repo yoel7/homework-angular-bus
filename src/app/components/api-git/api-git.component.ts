4574230d536f11fc355a3b2a3b4d27ee78e509d3
yoel7 <76557026+yoel7@users.noreply.github.com>
2021-02-22 18:31:16 +0200

    1

--- src/app/components/api-git/api-git.component.ts
+++ src/app/components/api-git/api-git.component.ts
@@ -10,11 +10,22 @@ export class ApiGitComponent implements OnInit {
 
   constructor( private htt: GitApiService) { }
   usergit: any;
+  _user:any;
   ngOnInit(): void {
   }
 gutGitUser(userName:HTMLInputElement):void{
+ 
+  
   this.htt.getU(userName.value).subscribe((a: any) => {
+    // this._user = a;
+    // console.log(typeof this._user.updated_at);
     this.usergit = a;
+    
+    // this._user.created_at=new Date(this._user.created_at)
+    // this._user.updated_at=new Date(this._user.updated_at)
+    // console.log(typeof this._user.updated_at);
+    // this.usergit = this._user;
+    // this._user.created_at=new Date(this._user.created_at)
   })
 }
 }
